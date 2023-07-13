b2688c3f9e6b6373ccc7041fdef25ca9cf3dc223
Ht-atho <thanh.opentechiz@gmail.com>
2023-07-13 09:24:57 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+  import React, { useState } from 'react';
+const MESSAGE_ERROR = {
+  username: "Username error",
+  email: "Email error",
+  password: "Password error",
+  confirmPassword: "Password must be the same"
+} 
+const REGEX = {
+  username: /^[a-zA-Z]{2,}$/,
+  email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+  password: /^[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]{6,}$/
 }
 
+function App() {     
+  const [email,setEmail] = useState()  
+  const checkAccount = (e)=>{
+    setEmail(e.target.value) 
+  }         
+  const checkLogin = ()=>{
+    if( !REGEX.email.test(email)){
+      alert(MESSAGE_ERROR.email)
+    }
+  }                                                                                                                               
+  return (
+    <div className="App">
+      <form> 
+        <h2>Đăng nhập </h2>
+        <label htmlFor="email"><h4 className='cuc'>Email:</h4></label>
+        <input className='cut' id='email'  onChange={checkAccount}  value={email} placeholder='email' />
+        <br />
+        <label htmlFor="password"><h4 className='cuc'>Password:</h4></label>
+        <input id='password' className='cut' type="password" placeholder='password' />
+        <br />
+        <button type='button' onClick={checkLogin} >Login</button>
+      </form>
+    </div>
+  );
+  }
 export default App;
